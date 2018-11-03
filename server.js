d90ebf33ba1012367593c8d4a8e613520145a599
Patrice Thomas <patrice.thomas1229@gmail.com>
2018-11-03 10:05:13 -0400

    sent request to google and dark sky api and received a response to the client

--- server.js
+++ server.js
@@ -5,10 +5,50 @@
 let express = require('express');
 let app = express (); //creating the variable app, and giving it the value of the invoked express function. Now I have access to the Express module, meaning that I have an instance of the Express API and therefore I can call its methods on this page. 
 
+// reuiring superagent libraby to be able to make Xhttp request to thirs party API's
+let superagent = require('superagent');
+
+//import cors to handle cross origin request
+let cors = require('cors');
+//invoking the method cors
+app.use(cors());
+
+//requiring the dotenv module and invoking the config method allowing us to add environment variables 
+   require('dotenv').config
+
+//checking if my route is working, when i send a request will i get a response.
+app.get('/',(request,response) => {
+    response.send('this route is working')
+})
 
 // creating a PORT for my server to listen to 
+let PORT = process.env.PORT  || 3000;
 
 
 
+//sending the request to access the third API, and getting the API response to us the client
+app.get('/location', (request,response) => {
+    let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=Port-au-Prince&key=AIzaSyCfYxZzwlvPhuUbZaVqHlW2HMkLt8Ol6eQ'
+    superagent.get(url)
+    .then(res => response.send({
+        latitude: res.body.results[0].geometry.location.lat,
+        longitude: res.body.results[0].geometry.location.lng
+      }))
+      .catch(err => response.send('<img src="http://http.cat/404" />'))
+      
+  })
+  
+//access a PORT
+app.listen(PORT,() => {
+    console.log(`server is running on PORT ${PORT}`)
+})
 
-
+//sending request to the weather third party API, to get me the response of the weather in PAP. 
+app.get('/weather',(request,response)=>{
+    let url1= 'https://api.darksky.net/forecast/f89425736b1ba654e69ef23694ce40ff/18.594395,-72.3074326'
+    superagent.get(url1)
+    .then(res => response.send({
+        hourly: res.body.hourly.summary,
+        daily: res.body.daily.summary,
+      }))
+})
